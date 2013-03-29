6aaa7ea6ea99ad556bf265e64d9b09a8a69d6481
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-03-29 18:58:48 -0400

    Work on the footer.

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
     <footer class="footer" role="contentinfo">
       <div id="footer-inner">
-        <nav role="navigation">
+        <div class="site-details">
+          <p><?php bloginfo('description'); ?></p>
+          <p class="copyright"><small>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('All rights reserved', 'sn'); ?>.</small></p>
+        </div>
+        <nav class="site-links" role="navigation">
           <?php sn_footer_links(); ?>
         </nav>
-        <p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
       </div>
     </footer>
 		<?php wp_footer(); ?>
